3d5abc7fbce54da643e8b2daf619981ed87f3fe8
2nthony <ant.hu@outlook.com>
2022-07-10 18:22:59 +0800

    feat: git update

--- src/git.ts
+++ src/git.ts
@@ -1,16 +1,23 @@
-import { spawn } from 'child_process'
+import { execSync, spawn as _spawn } from 'child_process'
 import path from 'path'
+import { promisify } from 'util'
 import { existsDir, makeDir } from './fs'
 import { rootPath } from './shared'
 import { analyzeUrl, composeUrl } from './shared/url'
 import { Repo } from './types'
 
-function cwd(repo: Repo) {
+const spawn = promisify(_spawn)
+
+function userCwd(repo: Repo) {
   return path.join(rootPath, repo.host, repo.user)
 }
 
+function repoCwd(repo: Repo) {
+  return path.join(userCwd(repo), repo.name)
+}
+
 function git(cmd: string, repo: Repo, ...options: string[]) {
-  spawn('git', [cmd, composeUrl(repo), ...options], {
+  return spawn('git', [cmd, composeUrl(repo), ...options], {
     cwd: path.join(rootPath, repo.host, repo.user),
     stdio: [process.stdin, process.stdout, process.stderr],
   })
@@ -19,9 +26,29 @@ function git(cmd: string, repo: Repo, ...options: string[]) {
 export async function clone(repoUrl: string, ...args: string[]) {
   const repo = analyzeUrl(repoUrl)
 
-  if (!(await existsDir(cwd(repo)))) {
-    await makeDir(cwd(repo))
+  if (!(await existsDir(userCwd(repo)))) {
+    await makeDir(userCwd(repo))
   }
 
   git('clone', repo, ...args)
+}
+
+export async function init(repoUrl: string, ...args: string[]) {
+  const repo = analyzeUrl(repoUrl)
+
+  // TODO: refactor this
+  if (!(await existsDir(repoCwd(repo)))) {
+    await makeDir(repoCwd(repo))
+  }
+
+  git('init', repo, ...args)
+}
+
+export function getUsername() {
+  try {
+    const stdout = execSync('git config --get user.name')
+    return stdout.toString().trim()
+  } catch {
+    return ''
+  }
 }
