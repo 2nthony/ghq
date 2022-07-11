dcb9438fca961cbf5937b69a4d0f7bd4c6ce4265
2nthony <ant.hu@outlook.com>
2022-07-11 23:14:50 +0800

    test: fs collect dirs

---
+++ test/fs.test.ts
@@ -0,0 +1,42 @@
+import { afterEach, beforeEach, describe, expect, test } from 'vitest'
+import os from 'os'
+import path from 'path'
+import fs from 'fs/promises'
+import { collectDirs, makeDir } from '../src/fs'
+
+describe('collect dirs', () => {
+  const tmpdir = os.tmpdir()
+  const test_dir_base = path.join(tmpdir, 'ghq')
+  const test_dir = path.join(test_dir_base, 'deep2', 'deep3', 'deep4')
+
+  beforeEach(async () => {
+    await makeDir(test_dir)
+  })
+
+  afterEach(async () => {
+    await fs.rm(path.join(test_dir_base), {
+      recursive: true,
+      force: true,
+    })
+  })
+
+  test('1', async () => {
+    const dirs = await collectDirs(test_dir_base)
+    expect(dirs).toContain(path.join(test_dir_base))
+  })
+
+  test('3', async () => {
+    const dirs = await collectDirs(test_dir_base, 3)
+    expect(dirs).toContain(path.join(test_dir_base, 'deep2', 'deep3'))
+  })
+
+  test('4', async () => {
+    const dirs = await collectDirs(test_dir_base, 4)
+    expect(dirs).toContain(path.join(test_dir_base, 'deep2', 'deep3', 'deep4'))
+  })
+
+  test('over deep will be empty', async () => {
+    const dirs = await collectDirs(test_dir_base, 5)
+    expect(dirs).toEqual([])
+  })
+})
