1b3d455f810326d2a1c4f77813191d04ec4ed8db
2nthony <ant.hu@outlook.com>
2022-07-11 15:20:13 +0800

    chore: update repo name example

--- src/commands/get.ts
+++ src/commands/get.ts
@@ -6,9 +6,9 @@ export const get: PluginApi = {
   extend(api) {
     api.cli
       .command('get [repo]', 'Clone/sync with a remote repository')
-      .example('ghq get 2nthony/ghq-node')
-      .example('ghq get github.com/2nthony/ghq-node')
-      .example('ghq get https://github.com/2nthony/ghq-node')
+      .example('ghq get 2nthony/ghq')
+      .example('ghq get github.com/2nthony/ghq')
+      .example('ghq get https://github.com/2nthony/ghq')
       .allowUnknownOptions()
       .action((repo, options) => {
         if (!repo) {
