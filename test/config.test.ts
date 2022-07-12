585c47c31066c988a630140fb83dd5ce734463a6
2nthony <ant.hu@outlook.com>
2022-07-12 23:16:48 +0800

    test: config

---
+++ test/config.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from 'vitest'
+import { readUserConfig, userConfigFilePath } from '../src/config'
+import { exists } from '../src/fs'
+
+describe('user config', async () => {
+  const existsConfigFile = await exists(userConfigFilePath)
+
+  test.runIf(existsConfigFile)('assert config', async () => {
+    const config = await readUserConfig()
+
+    expect(config).toBeTypeOf('object')
+  })
+
+  test.runIf(!existsConfigFile)('no config file', async () => {
+    const config = await readUserConfig()
+
+    expect(config).toEqual({})
+  })
+})
