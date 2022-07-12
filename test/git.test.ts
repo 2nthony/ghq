0518d951ac2ab0586485da5cf58afbbfb54f061e
2nthony <ant.hu@outlook.com>
2022-07-12 22:59:36 +0800

    test: git

--- test/git.test.ts
+++ test/git.test.ts
@@ -1,25 +1,33 @@
 import path from 'path'
 import { describe, expect, test } from 'vitest'
+import { resolveConfig } from '../src/config'
 import { repoDest, username } from '../src/git'
-import { rootPath } from '../src/shared'
 import { analyzeUrl } from '../src/shared/url'
 
-describe('repo dest', () => {
-  test('repo', () => {
+describe('repo dest', async () => {
+  const { root } = await resolveConfig()
+
+  test('repo', async () => {
     const repo = analyzeUrl('ghq')
 
-    expect(repoDest(repo)).toBe(path.join(rootPath, repo.host, username, 'ghq'))
+    expect(await repoDest(repo)).toBe(
+      path.join(root, repo.host, username, 'ghq'),
+    )
   })
 
-  test('user/repo', () => {
+  test('user/repo', async () => {
     const repo = analyzeUrl(`${username}/ghq`)
 
-    expect(repoDest(repo)).toBe(path.join(rootPath, repo.host, username, 'ghq'))
+    expect(await repoDest(repo)).toBe(
+      path.join(root, repo.host, username, 'ghq'),
+    )
   })
 
-  test('protocol://domain/user/repo', () => {
+  test('protocol://domain/user/repo', async () => {
     const repo = analyzeUrl(`https://github.com/${username}/ghq`)
 
-    expect(repoDest(repo)).toBe(path.join(rootPath, repo.host, username, 'ghq'))
+    expect(await repoDest(repo)).toBe(
+      path.join(root, repo.host, username, 'ghq'),
+    )
   })
 })
