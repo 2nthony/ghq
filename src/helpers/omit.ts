85b6beb6bed3257a3e3865bfd6270d29cbfbe050
2nthony <ant.hu@outlook.com>
2022-07-14 02:37:40 +0800

    feat: omit helper

---
+++ src/helpers/omit.ts
@@ -0,0 +1,10 @@
+export function omit<T extends object, K extends keyof T>(
+  obj: T,
+  keys: K[],
+): Omit<T, K> {
+  const newObj = { ...obj }
+
+  keys.forEach((k) => delete newObj[k])
+
+  return newObj
+}
