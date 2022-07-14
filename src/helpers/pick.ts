9f6923762e4f19a305a6e9d9fd4a41e52a607de5
2nthony <ant.hu@outlook.com>
2022-07-15 02:39:21 +0800

    fix: pick, set truthy only

--- src/helpers/pick.ts
+++ src/helpers/pick.ts
@@ -3,7 +3,9 @@ export function pick<T extends object, K extends keyof T>(
   keys: K[],
 ): Pick<T, K> {
   return keys.reduce((val, k) => {
-    val[k] = obj[k]
+    if (obj[k]) {
+      val[k] = obj[k]
+    }
     return val
   }, {} as T)
 }
