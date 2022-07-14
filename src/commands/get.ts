045617a5ff41c8573d01fe7ebb6f421fff97e53f
2nthony <ant.hu@outlook.com>
2022-07-15 02:41:19 +0800

    fix(command): get apply ghqrc

--- src/commands/get.ts
+++ src/commands/get.ts
@@ -1,25 +1,30 @@
 import { clone } from '../git'
 import { parseCliOptionsToGitArgs } from '../args'
 import { PluginApi } from '../types'
+import { readConfig } from '../config'
 
 export const get: PluginApi = {
   extend(api) {
     api.cli
       .command('get [repo]', 'Clone/sync with a remote repository')
       .alias('clone')
-      .option('--shallow', 'Shallow clone, alias to `--depth 1`')
+      .option('--shallow', 'Shallow clone, alias to `--depth 1`', {
+        default: false,
+        type: [Boolean],
+      })
       .example('ghq get 2nthony/ghq')
       .example('ghq get github.com/2nthony/ghq')
       .example('ghq get https://github.com/2nthony/ghq')
       .example('ghq clone 2nthony/ghq')
       .allowUnknownOptions()
-      .action((repo, options) => {
+      .action(async (repo, options) => {
         if (!repo) {
           api.cli.outputHelp()
           return
         }
 
-        const args = parseCliOptionsToGitArgs(options)
+        const config = await readConfig()
+        const args = parseCliOptionsToGitArgs({ ...config, ...options })
 
         clone(repo, ...args)
       })
